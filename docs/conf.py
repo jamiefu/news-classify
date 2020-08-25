7e300bde901f04b25e73fa6f0701c40a7329f582
Jamie Fu <jamie.fu@gmail.com>
2020-08-25 00:20:53 -0400

    bye bye contents.rst

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = 'Arun Wongprommoon, Jamie Fu, Haimoshri Das, Jianna Liu, Max Tegmark'
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
